f77ce56fc66956b39012b5af9053eb62d352ad10
AhmedSamy02 <nname1858@gmail.com>
2025-06-17 21:06:01 +0300

    [📦️ PKG] Adding firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nawel/core/constants/screens.dart';
+import 'package:nawel/features/auth/presentation/controllers/auth_bloc.dart';
 import 'package:nawel/features/auth/presentation/screens/login_screen.dart';
 import 'package:nawel/features/auth/presentation/screens/register_screen.dart';
 import 'package:nawel/features/onboarding/landing_screen.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'features/splash_screen.dart';
 
-void main() {
+void main() async{
   GetIt.I.registerSingleton<FlutterSecureStorage>(const FlutterSecureStorage());
-  WidgetsFlutterBinding.ensureInitialized();
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);  
   runApp(const MainApp());
 }
 
@@ -28,7 +34,10 @@ class MainApp extends StatelessWidget {
         routes: {
           kSplashScreen: (context) => const SplashScreen(),
           kLandingScreen: (context) => LandingScreen(),
-          kLoginScreen: (context) => LoginScreen(),
+          kLoginScreen: (context) => BlocProvider(
+                create: (context) => AuthBloc(),
+                child: LoginScreen(),
+              ),
           kRegisterScreen: (context) => RegisterScreen(),
         },
         initialRoute: kLoginScreen,
